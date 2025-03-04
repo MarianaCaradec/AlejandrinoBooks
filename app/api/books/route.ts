6f18ae76f3d43db30599160a12bbf797a913b74b
Mariana Caradec <maricaradec@gmail.com>
2025-03-04 18:19:06 -0300

    feat: added pagination to GET request

--- app/api/books/route.ts
+++ app/api/books/route.ts
@@ -1,21 +1,39 @@
-import { NextResponse } from "next/server"
-import {BookWithCategory, prisma} from '@/lib/prisma'
+import { NextRequest, NextResponse } from "next/server"
+import {prisma} from '@/lib/prisma'
 
-export async function GET() {
-try {
-    const booksFromDb = await prisma.book.findMany({
-        include: {
-            category: true
-        }
-    })
+export async function GET(req: NextRequest) {
+    if(req.method === "GET") {
+        try {
+            const { searchParams } = new URL(req.url);
+            const pageInt = parseInt(searchParams.get("page") || "1", 10);
+            const limitInt = parseInt(searchParams.get("limit") || "10", 10);
+        
+            if (isNaN(pageInt) || isNaN(limitInt) || pageInt < 1 || limitInt < 1) {
+                return NextResponse.json({ error: "Parámetros inválidos" }, { status: 400 });
+            }
 
-    return NextResponse.json(booksFromDb, {status: 200})
-} catch (error){
-    return NextResponse.json(
-        {error: "Error al obtener los libros"},
-        {status: 500}
-    )
-}
+            const booksFromDb = await prisma.book.findMany({
+                skip: (pageInt - 1) * limitInt,
+                take: limitInt,
+            })
+
+            const totalBooks = await prisma.book.count()
+
+            const totalPages = Math.ceil(Number(totalBooks) / limitInt)
+        
+            return NextResponse.json({
+                booksFromDb,
+                totalBooks,
+                totalPages,
+                currentPage: pageInt
+            }, {status: 200})
+        } catch (error){
+            return NextResponse.json(
+                { error: error instanceof Error ? error.message : "Error desconocido al encontrar los libros" },
+                {status: 500}
+            )
+        }   
+    }
 }
 
 export async function POST(req: Request) {
