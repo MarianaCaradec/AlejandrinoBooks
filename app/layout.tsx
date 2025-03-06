13d921e3998d902e6ebeaca39b27e594e1029fca
Mariana Caradec <maricaradec@gmail.com>
2025-03-06 18:35:01 -0300

    style: added styles to children

--- app/layout.tsx
+++ app/layout.tsx
@@ -28,8 +28,8 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <Navbar/>
-        {children}
+        <Navbar />
+        <main className="relative">{children}</main>
       </body>
     </html>
   );
