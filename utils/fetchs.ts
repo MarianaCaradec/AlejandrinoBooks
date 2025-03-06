62e3015fc8c69fbdb7289e2d8010cf993580f382
Mariana Caradec <maricaradec@gmail.com>
2025-03-06 12:31:43 -0300

    feat: created fetch function for booksByCategory

--- utils/fetchs.ts
+++ utils/fetchs.ts
@@ -1,15 +1,27 @@
 import { BookWithCategory, CategoryWithBooks } from "@/lib/prisma";
 
-const API_URL = process.env.NEXT_PUBLIC_API_URL || "http://localhost:3000";
+const API_URL = process.env.NEXT_PUBLIC_API_URL
 
 export const fetchBooks = async (page = 1, limit = 5) => {
     try {
         const res = await fetch(`${API_URL}/api/books?page=${page}&limit=${limit}`);
-        const booksPagination = await res.json();
+        const booksPagination = await res.json()
         return booksPagination
         } catch (error) {
         console.error("Error fetching books:", error);
+        return []
     };
+}
+
+export const fetchBooksByCategory = async (categoryId: string): Promise<BookWithCategory[] | null> => {
+    try {
+        const res = await fetch(`${API_URL}/api/books/byCategory?categoryId=${categoryId}`)
+        const booksByCategory: BookWithCategory[] = await res.json()
+        return booksByCategory
+    } catch (error) {
+        console.error("Error fetching books by category:", error);
+        return []
+    }
 }
 
 export const fetchBook = async (bookId: string): Promise<BookWithCategory | null> => {
@@ -23,13 +35,14 @@ export const fetchBook = async (bookId: string): Promise<BookWithCategory | null
     }
 };
 
-export const fetchCategories = async () => {
+export const fetchCategories = async (): Promise<CategoryWithBooks[] | null> => {
     try {
         const res = await fetch("/api/categories");
-        const categoriesFromDb = await res.json();
+        const categoriesFromDb: CategoryWithBooks[] = await res.json();
         return categoriesFromDb
     } catch (error) {
         console.error("Error fetching categories:", error);
+        return []
     } 
 }
 
