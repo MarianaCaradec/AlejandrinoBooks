a5f8fa38e49dbcaa330af81d7d274f3e8ffd698b
Mariana Caradec <maricaradec@gmail.com>
2025-03-12 18:40:44 -0300

    feat: added searchParams for categoryId and search by title, author or category, checking return to avoid errors

--- app/api/books/route.ts
+++ app/api/books/route.ts
@@ -1,29 +1,52 @@
 import { NextRequest, NextResponse } from "next/server"
 import {prisma} from '@/lib/prisma'
+import { Prisma } from "@prisma/client";
 
 export async function GET(req: NextRequest) {
         try {
             const { searchParams } = new URL(req.url);
+
             const pageInt = parseInt(searchParams.get("page") || "1", 10);
             const limitInt = parseInt(searchParams.get("limit") || "5", 10);
+
+            const wordToBeSearched = searchParams.get("search")?.trim() || ""
+            const categoryId = searchParams.get("categoryId") || ""
         
             if (isNaN(pageInt) || isNaN(limitInt) || pageInt < 1 || limitInt < 1) {
                 return NextResponse.json({ error: "Parámetros inválidos" }, { status: 400 });
             }
 
+            const whereConditions: Prisma.BookWhereInput = {}
+
+            if (categoryId) {
+                whereConditions.categoryId = categoryId
+            }
+
+            if (wordToBeSearched) {
+                whereConditions.OR = [
+                        {title: {contains: wordToBeSearched}}, 
+                        {author: {contains: wordToBeSearched}},
+                        {category: {name: {contains: wordToBeSearched}}}
+                ]
+            }
+
             const booksFromDb = await prisma.book.findMany({
+                include: {
+                    category: true
+                },
+                where: whereConditions,
                 skip: (pageInt - 1) * limitInt,
                 take: limitInt
-            })
+            }) ?? []
 
-            const totalBooks = await prisma.book.count()
+            const totalBooks = await prisma.book.count({ where: whereConditions })
 
             const totalPages = Math.ceil(Number(totalBooks) / limitInt)
         
             return NextResponse.json({
-                booksFromDb,
-                totalBooks,
-                totalPages,
+                booksFromDb: booksFromDb ?? [],
+                totalBooks: totalBooks ?? 0,
+                totalPages: totalPages ?? 1,
                 currentPage: pageInt
             }, {status: 200})
         } catch (error){
@@ -52,7 +75,7 @@ export async function POST(req: Request) {
     return NextResponse.json(newBook, {status: 201})
     } catch (error) {
         return NextResponse.json(
-            {error: "Error al crear un libro nuevo"},
+            { error: error instanceof Error ? error.message : "Error al crear un libro nuevo"},
             {status: 500}
         )
     }
