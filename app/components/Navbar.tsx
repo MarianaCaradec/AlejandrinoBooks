2cf5c6825be54b4f98b51f366c4452336aef206f
Mariana Caradec <maricaradec@gmail.com>
2025-03-17 13:05:20 -0300

    feat: added authentication on navbar to show only the needed buttons for the user

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,6 +1,27 @@
+"use client";
 import Link from "next/link";
+import { useEffect, useState } from "react";
+import { usePathname } from "next/navigation";
+import { logout } from "../actions/logout";
+import { fetchAuth } from "@/utils/fetchs";
 
 export default function Navbar() {
+  const [isAuthenticated, setIsAuthenticated] = useState<boolean>(false);
+  const pathName = usePathname();
+
+  useEffect(() => {
+    const getAuth = async () => {
+      try {
+        const data = await fetchAuth();
+        if (data) setIsAuthenticated(data.isAuthenticated);
+      } catch (error) {
+        console.error("Error al confirmar autenticación del usuario", error);
+      }
+    };
+
+    getAuth();
+  }, [pathName]);
+
   return (
     <nav className="bg-black shadow-md p-4 flex justify-between items-center fixed top-0 left-0 w-full z-50 bg-black/70 backdrop-blur-md">
       <div>
@@ -15,9 +36,27 @@ export default function Navbar() {
         <Link href="/books" className="text-[#E4DFDA] hover:text-[#53917E]">
           Books
         </Link>
-        <Link href="/profile" className="text-[#E4DFDA] hover:text-[#53917E]">
-          My profile
-        </Link>
+        {isAuthenticated ? (
+          <>
+            <Link
+              href="/profile"
+              className="text-[#E4DFDA] hover:text-[#53917E]"
+            >
+              Profile
+            </Link>
+            <button
+              type="submit"
+              onClick={logout}
+              className="text-[#E4DFDA] hover:text-[#53917E] font-semibold"
+            >
+              Log out
+            </button>
+          </>
+        ) : (
+          <Link href="/signin" className="text-[#E4DFDA] hover:text-[#53917E]">
+            Sign in
+          </Link>
+        )}
       </div>
     </nav>
   );
