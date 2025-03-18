51c0eec5bc79a630387c465227e6f937dd43988d
Mariana Caradec <maricaradec@gmail.com>
2025-03-18 19:41:53 -0300

    feat: added user data to GET authentication to managing all the data at once

--- app/api/auth/me/route.ts
+++ app/api/auth/me/route.ts
@@ -1,3 +1,4 @@
+import { prisma } from "@/app/lib/prisma";
 import jwt from "jsonwebtoken";
 import { cookies } from "next/headers";
 import { NextResponse } from "next/server";
@@ -6,13 +7,25 @@ export async function GET() {
     const token = (await cookies()).get("token")?.value
 
     if(!token) {
-        return NextResponse.json({isAuthenticated: false})
+        return NextResponse.json({isAuthenticated: false, user: null}, {status: 401})
     }
 
     try {
-        jwt.verify(token, process.env.JWT_SECRET!)
-        return NextResponse.json({isAuthenticated: true})
+        const decodedToken = jwt.verify(token, process.env.JWT_SECRET!) as {email: string}
+        
+        const user = await prisma.user.findUnique({
+            where: {email: decodedToken.email},
+            select: { id: true, name: true, email: true, role: true }})
+
+        if (!user) {
+            return NextResponse.json({ isAuthenticated: false, user: null }, { status: 401 });
+        }
+
+        return NextResponse.json({
+            isAuthenticated: true, 
+            user}, 
+            {status: 200})
     } catch (error) {
-        return NextResponse.json({isAuthenticated: false})
+        return NextResponse.json({isAuthenticated: false, user: null}, {status: 401})
     }
 }
