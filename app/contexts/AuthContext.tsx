93293b09fa0535bd1207d2af07b628dfa2af812c
Mariana Caradec <maricaradec@gmail.com>
2025-03-20 19:45:31 -0300

    refactor: changed lenguage on error message

--- app/contexts/AuthContext.tsx
+++ app/contexts/AuthContext.tsx
@@ -44,6 +44,6 @@ export function AuthProvider({ children }: { children: React.ReactNode }) {
 
 export function useAuth() {
   const context = useContext(AuthContext);
-  if (!context) throw new Error("useAuth debe usarse dentro de AuthProvider");
+  if (!context) throw new Error("useAuth must be used inside AuthProvider");
   return context;
 }
