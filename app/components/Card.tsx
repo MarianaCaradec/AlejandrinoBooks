860b275c0296b0bc4ebb31c3dd87722aa20d7cef
Mariana Caradec <maricaradec@gmail.com>
2025-03-23 18:23:03 -0300

    feat: improved Card's component to handle case of avatar null

--- app/components/Card.tsx
+++ app/components/Card.tsx
@@ -2,22 +2,29 @@ import Image from "next/image";
 
 interface AccountProps {
   name: string;
-  avatar: string;
+  avatar?: string;
   email: string;
   desc: string;
 }
 
 export default function Card({ name, avatar, email, desc }: AccountProps) {
+  const bucketUrl = process.env.BUCKET_PROFILE_IMGS_URL;
+  const defaultAvatarUrl = `${bucketUrl}/default_profile_pic.jpeg`;
+  const validAvatar = avatar || defaultAvatarUrl;
+
   return (
     <div className="flex flex-col items-center justify-center">
       <h1 className="mt-4 text-xl font-semibold text-[#D4B483]">{name}</h1>
-      <Image
-        src={avatar}
-        alt="Profile photo"
-        width="200"
-        height="300"
-        className="object-cover mt-4 rounded-lg"
-      />
+      {avatar && (
+        <Image
+          src={validAvatar}
+          alt="Profile photo"
+          width={200}
+          height={300}
+          priority={true}
+          className="object-cover mt-4 rounded-lg"
+        />
+      )}
       <h2 className="text-[#D4B483] p-3">{email}</h2>
       <p className="text-center text-[#E4DFDA]">{desc}</p>
     </div>
