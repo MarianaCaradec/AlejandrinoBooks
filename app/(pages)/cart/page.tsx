3df86241d01eee324d3628b9cd92f85713a5d88d
Mariana Caradec <maricaradec@gmail.com>
2025-03-29 11:39:54 -0300

    feat: just created cart page -lacks of real data

---
+++ app/(pages)/cart/page.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+export default function page() {
+  return (
+    <div className="max-w-4xl mx-auto p-6 py-20">
+      <h2 className="text-3xl text-center font-bold text-[#D4B483] mb-6">
+        Welcome to your cart
+      </h2>
+      <h3 className="text-2xl text-center font-bold text-[#D4B483] mb-6">
+        These are your current items:
+      </h3>
+    </div>
+  );
+}
