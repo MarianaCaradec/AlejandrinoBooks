9f46b0b05a7424f2f9059564d40997ea3b174923
Mariana Caradec <maricaradec@gmail.com>
2025-03-31 02:27:52 -0300

    fix: changed managment of images for showing correctly bucket's files

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
-    domains: ['randomuser.me', 'storage.cloud.google.com', 'storage.googleapis.com'],
+    unoptimized: true,
   },
   eslint: {
     ignoreDuringBuilds: true,
