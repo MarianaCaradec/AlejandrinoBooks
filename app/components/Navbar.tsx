3a566cecb729d2dcb4a3827ae62ca0342001b8ad
Mariana Caradec <maricaradec@gmail.com>
2025-04-04 15:43:17 -0300

    refactor: removed import of unused logout action -it's inside of logoutHandler of log out button

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,6 +1,5 @@
 "use client";
 import Link from "next/link";
-import { logout } from "../actions/logout";
 import { useAuth } from "../contexts/AuthContext";
 import CartIcon from "./CartIcon";
 import { useEffect } from "react";
