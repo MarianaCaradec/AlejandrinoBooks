021fee9fc9fc1c997cc6af06bd23e996fc5b7871
Mariana Caradec <155834936+MarianaCaradec@users.noreply.github.com>
2025-04-04 16:08:46 -0300

    Revert "Revert "feat: added check for expiration of token""
    This reverts commit f41402242e93181505fdc6fd96b1d7c463add6be.

--- app/api/auth/me/route.ts
+++ app/api/auth/me/route.ts
@@ -1,24 +1,37 @@
 import { prisma } from "@/app/lib/prisma";
 import { jwtVerify } from "jose";
 import { cookies } from "next/headers";
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
-export async function GET() {
-    const token = (await cookies()).get("token")?.value
-    const JWT_SECRET = new TextEncoder().encode(process.env.JWT_SECRET!);
-
-    if(!token) {
-        return NextResponse.json({isAuthenticated: false, user: null}, {status: 401})
-    }
-
+export async function GET(req: NextRequest) {
     try {
+        const token = (await cookies()).get("token")?.value;
+        console.log("Server-side request headers:", req.headers);
+
+        if(!token) {
+            console.error("No authentication token found.");
+            return NextResponse.json({isAuthenticated: false, user: null}, {status: 401})
+        }
+
+        const JWT_SECRET = new TextEncoder().encode(process.env.JWT_SECRET!);
         const {payload} = await jwtVerify<{email: string}>(token, JWT_SECRET)
+        console.log("Decoded payload:", payload);
+
+                const isTokenExpired = (exp?: number): boolean => {
+                    if (!exp) return true;
+                    return Math.floor(Date.now() / 1000) > exp;
+                }  
+        
+                if (isTokenExpired(payload.exp)) {
+                    return NextResponse.redirect("/login"); 
+                }
 
         const user = await prisma.user.findUnique({
             where: {email: payload.email},
             select: { id: true, name: true, email: true, image: true, role: true }})
 
         if (!user) {
+            console.error("User not found in database.");
             return NextResponse.json({ isAuthenticated: false, user: null }, { status: 401 });
         }
 
