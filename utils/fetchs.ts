5977e13a17b9f38ad7ca31fa6001fb4bd30e093b
Mariana Caradec <maricaradec@gmail.com>
2025-04-07 19:24:18 -0300

    fix: removed env var or API_URL

--- utils/fetchs.ts
+++ utils/fetchs.ts
@@ -1,13 +1,16 @@
 import { BookWithCategory, CategoryWithBooks } from "@/app/lib/prisma";
+import { AuthError, DatabaseError } from "@/errorHandler";
 
-const API_URL = process.env.NEXT_PUBLIC_API_URL
+// const API_URL = process.env.NEXT_PUBLIC_API_URL
 
 export const fetchBooks = async (page = 1, limit = 5, categoryId?: string, search?: string) => {
     try {
         const categoryQuery = categoryId ? `&categoryId=${categoryId}` : "";
         const searchQuery = search ? `&search=${search}` : "";
 
-        const res = await fetch(`${API_URL}/api/books?page=${page}&limit=${limit}${categoryQuery}${searchQuery}`);
+        const res = await fetch(`/api/books?page=${page}&limit=${limit}${categoryQuery}${searchQuery}`);
+
+        if (!res.ok) throw new DatabaseError();
         
         const booksPagination = await res.json()
         return booksPagination
@@ -19,7 +22,10 @@ export const fetchBooks = async (page = 1, limit = 5, categoryId?: string, searc
 
 export const fetchBook = async (bookId: string): Promise<BookWithCategory | null> => {
     try {
-        const res = await fetch(`${API_URL}/api/books/${bookId}`);
+        const res = await fetch(`/api/books/${bookId}`);
+
+        if (!res.ok) throw new DatabaseError();
+
         const bookFromDb: BookWithCategory = await res.json();
         return bookFromDb
     } catch (error) {
@@ -30,7 +36,10 @@ export const fetchBook = async (bookId: string): Promise<BookWithCategory | null
 
 export const fetchCategories = async (): Promise<CategoryWithBooks[] | null> => {
     try {
-        const res = await fetch(`${API_URL}/api/categories`);
+        const res = await fetch(`/api/categories`);
+
+        if (!res.ok) throw new DatabaseError();
+
         const categoriesFromDb: CategoryWithBooks[] = await res.json();
         return categoriesFromDb
     } catch (error) {
@@ -41,7 +50,10 @@ export const fetchCategories = async (): Promise<CategoryWithBooks[] | null> => 
 
 export const fetchCategory = async (categoryId: string): Promise<CategoryWithBooks | null> => {
     try {
-        const res = await fetch(`${API_URL}/api/categories/${categoryId}`)
+        const res = await fetch(`/api/categories/${categoryId}`)
+
+        if (!res.ok) throw new DatabaseError();
+
         const categoryFromDb: CategoryWithBooks = await res.json()
         return categoryFromDb
     } catch (error) {
@@ -52,20 +64,19 @@ export const fetchCategory = async (categoryId: string): Promise<CategoryWithBoo
 
 export const fetchAuth = async () => {
     try {
-        const res = await fetch(`${API_URL}/api/auth/me`, { 
+        const res = await fetch(`/api/auth/me`, { 
             cache: "no-store",       
             credentials: "include",
             headers: {"Content-Type": "application/json",} 
         });
 
-        if (res.status === 401) {
-            return null;
-        }
-        if (!res.ok) throw new Error("Failed to fetch user data");
+        if (res.status === 401) throw new AuthError()
+
+        if (!res.ok) throw new DatabaseError();
 
         const auth = await res.json();
 
-        if (!auth.user || !auth.user.id) throw new Error("User ID is missing")
+        if (!auth.user || !auth.user.id) throw new AuthError("User ID is missing")
         
         return auth
     } catch (error) {
@@ -76,12 +87,11 @@ export const fetchAuth = async () => {
 
 export const fetchCart = async () => {
     try {
-        const res = await fetch(`${API_URL}/api/cart`, {credentials: "include"})
+        const res = await fetch(`/api/cart`, {credentials: "include"})
 
-        if (res.status === 401) {
-            return null;
-        }
-        if (!res.ok) throw new Error("Failed to fetch cart");
+        if (res.status === 401) throw new AuthError()
+
+        if (!res.ok) throw new DatabaseError();
 
         const cart = await res.json()
         return cart.cartItems || []
