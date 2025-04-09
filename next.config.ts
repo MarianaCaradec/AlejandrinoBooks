5fe27a1b7ac9f940542489698a92a5a86817945a
Mariana Caradec <maricaradec@gmail.com>
2025-04-09 20:53:19 -0300

    feat: added mercado pago's env vars for correct rendering of them to pay

--- next.config.ts
+++ next.config.ts
@@ -8,6 +8,10 @@ const nextConfig: NextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
+  env: {
+    MERCADOPAGO_PUBLIC_KEY: process.env.MERCADO_PAGO_PUBLIC_KEY,
+    MERCADOPAGO_ACCESS_TOKEN: process.env.MERCADO_PAGO_ACCESS_TOKEN,
+  },
 };
 
 export default nextConfig;
