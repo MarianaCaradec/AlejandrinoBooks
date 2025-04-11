9224186a380b5f140c84a390a091f4c26d7884fc
Mariana Caradec <maricaradec@gmail.com>
2025-04-11 20:16:41 -0300

    feat: added endpoint for update the order if payment succeded

---
+++ app/api/payment/completed/route.ts
@@ -0,0 +1,22 @@
+import { prisma } from "@/app/lib/prisma";
+import { NextResponse } from "next/server";
+
+export async function POST(req: Request) {
+    try {
+      const { orderId } = await req.json();
+  
+      if (!orderId) {
+        return NextResponse.json({ error: "Missing orderId." }, { status: 400 });
+      }
+  
+      await prisma.order.update({
+        where: { id: orderId },
+        data: { status: "COMPLETED" },
+      });
+  
+      return NextResponse.json({ message: "Order status updated successfully." });
+    } catch (error) {
+      console.error("Error updating order status:", error);
+      return NextResponse.json({ error: "Internal Server Error" }, { status: 500 });
+    }
+  }
