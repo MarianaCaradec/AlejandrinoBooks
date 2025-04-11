8439dbe18e35ad9d6363909d3ccefab1776537bf
Mariana Caradec <maricaradec@gmail.com>
2025-04-11 20:14:18 -0300

    feat: added endpoint for checking the status of payment

---
+++ app/api/payment/status/route.ts
@@ -0,0 +1,42 @@
+import mercadopago from "../../../../utils/mercadoPago";
+import { Payment } from 'mercadopago';
+import { NextResponse } from 'next/server';
+
+export async function GET(req: Request) {
+  try {
+    const { searchParams } = new URL(req.url);
+    const paymentId = searchParams.get('payment_id');
+
+    if (!paymentId) {
+      return new Response(
+        JSON.stringify({ error: 'Missing paymentId in query params.' }),
+        { status: 400, headers: { 'Content-Type': 'application/json' } }
+      );
+    }
+
+    const payment = await new Payment(mercadopago).get({ id: paymentId });
+
+    if (!payment) {
+      return new Response(
+        JSON.stringify({ error: 'Payment not found.' }),
+        { status: 404, headers: { 'Content-Type': 'application/json' } }
+      );
+    }
+
+    
+    const { status, metadata, transaction_amount } = payment;
+
+    return new Response(
+        JSON.stringify({
+        status: status,
+        metadata: metadata, 
+        transaction_amount:transaction_amount,
+      }),
+      { status: 200, headers: { 'Content-Type': 'application/json' } }
+    );
+
+} catch (error) {
+        console.error("Error processing payment notification:", error);
+        return NextResponse.json({ error: "Internal Server Error" }, { status: 500 });
+}
+}
