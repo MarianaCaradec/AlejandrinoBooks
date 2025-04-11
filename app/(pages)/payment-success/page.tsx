2d861e56e819018688b0a4315f45bf716e6cf915
Mariana Caradec <maricaradec@gmail.com>
2025-04-11 20:17:46 -0300

    feat: added success page to redirect the user after pay, update the order and empty the cart

---
+++ app/(pages)/payment-success/page.tsx
@@ -0,0 +1,55 @@
+import { useCartItemContext } from "@/app/contexts/CartItemContext";
+import Link from "next/link";
+import { useEffect } from "react";
+
+const page = () => {
+  const { setCartItems } = useCartItemContext();
+
+  const checkPaymentStatus = async () => {
+    const urlParams = new URLSearchParams(window.location.search);
+    const paymentId = urlParams.get("payment_id");
+
+    if (paymentId) {
+      const payment = await fetch(
+        `/api/payment/status?paymentId=${paymentId}`,
+        {
+          method: "GET",
+          headers: { "Content-Type": "application/json" },
+        }
+      );
+      const paymentData = await payment.json();
+
+      if (paymentData.status === "approved") {
+        await fetch("/api/payment/completed", {
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify({ orderId: paymentData.metadata.orderId }),
+        });
+
+        setCartItems([]);
+      } else {
+        console.log("Payment not approved yet or failed.");
+      }
+    }
+  };
+
+  useEffect(() => {
+    checkPaymentStatus();
+  }, []);
+
+  return (
+    <div>
+      <h1>Payment Success</h1>
+      <p>Your payment was successful!</p>
+      <p>Thank you for your purchase.</p>
+      <Link
+        href={"/"}
+        className="bg-[#53917E] text-black font-bold px-4 py-2 rounded-md hover:bg-red-600 transition"
+      >
+        Go back to landing page
+      </Link>
+    </div>
+  );
+};
+
+export default page;
