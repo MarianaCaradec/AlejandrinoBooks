059e40eb394693f51c621218bc41837bb2907007
Mariana Caradec <maricaradec@gmail.com>
2025-04-12 13:18:08 -0300

    feat: added fallback of url param's orderId

--- app/api/payment/status/route.ts
+++ app/api/payment/status/route.ts
@@ -6,6 +6,7 @@ export async function GET(req: Request) {
   try {
     const { searchParams } = new URL(req.url);
     const paymentId = searchParams.get('payment_id');
+    const fallbackOrderId = searchParams.get('order_id');
 
     if (!paymentId) {
       return new Response(
@@ -22,18 +23,17 @@ export async function GET(req: Request) {
         { status: 404, headers: { 'Content-Type': 'application/json' } }
       );
     }
-
     
     const { status, transaction_amount } = payment;
     const metadata = payment?.metadata ?? {};
-    const orderId = metadata.orderId ?? null;
+    const orderId = metadata.orderId ?? fallbackOrderId ?? null;
 
     console.log("The orderId and status are: ", metadata.orderId, metadata.status);
     return new Response(
         JSON.stringify({
-        status: status,
-        orderId: orderId, 
-        transaction_amount: transaction_amount,
+        status,
+        orderId, 
+        transaction_amount,
       }),
       { status: 200, headers: { 'Content-Type': 'application/json' } }
     );
